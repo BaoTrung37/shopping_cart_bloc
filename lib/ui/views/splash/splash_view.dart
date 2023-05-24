0c4832a1ddbdf723c8632d9b537794258b0e545d
BaoTrung <>
2023-05-24 19:56:22 +0700

    init splash view

---
+++ lib/ui/views/splash/splash_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Image.asset(
+                  'assets/cart.png',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+            ),
+            const Text('@ 2023, QSoft. All rights reserved.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
